7dab1dc32306bd0c6d378ea738b3c2e489b49ff1
NyaNyagulugulu <support@cnmsb.xin>
2026-04-07 01:45:26 +0800

    标准化替换

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -25,7 +25,7 @@
         
         <div class="sidebar-playlists">
         <div class="playlists-header">
-          <span class="playlists-title">{{ t('sidebar.myPlaylists') }}</span>
+          <span class="playlists-title">{{ t('key.myPlaylists') }}</span>
           <button class="add-playlist-btn" @click="showCreatePlaylistModal = true" :title="t('playlists.createPlaylist')">
             <svg viewBox="0 0 24 24" width="16" height="16">
               <path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
@@ -53,7 +53,7 @@
       <div v-if="favoritePlaylists.length > 0" class="sidebar-playlists">
         <div class="nav-divider"></div>
         <div class="playlists-header">
-          <span class="playlists-title">{{ t('sidebar.favoritePlaylists') }}</span>
+          <span class="playlists-title">{{ t('key.favoritePlaylists') }}</span>
         </div>
         <div class="playlists-list">
           <div 
@@ -293,10 +293,10 @@ const toasts = ref([])
 let toastId = 0
 
 const navItems = computed(() => [
-  { key: 'home', label: t('sidebar.home'), icon: 'M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z' },
+  { key: 'home', label: t('key.home'), icon: 'M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z' },
   { divider: true },
-  { key: 'favorites', label: t('sidebar.favorites'), icon: 'M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z' },
-  { key: 'recent', label: t('sidebar.recentPlay'), icon: 'M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z' },
+  { key: 'favorites', label: t('key.favorites'), icon: 'M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z' },
+  { key: 'recent', label: t('key.recentPlay'), icon: 'M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z' },
   { divider: true }
 ])
 
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -20,7 +20,7 @@
         
         <!-- 我的歌单 -->
         <div v-if="myPlaylists.length > 0" class="playlists-group">
-          <div class="playlists-group-title">{{ t('sidebar.myPlaylistsTitle') }}</div>
+          <div class="playlists-group-title">{{ t('key.myPlaylistsTitle') }}</div>
           <div 
             v-for="playlist in myPlaylists.slice(0, 5)" 
             :key="`my-${playlist.id}`"
@@ -33,7 +33,7 @@
         
         <!-- 收藏的歌单 -->
         <div v-if="favoritePlaylists.length > 0" class="playlists-group">
-          <div class="playlists-group-title">{{ t('sidebar.favoritePlaylistsTitle') }}</div>
+          <div class="playlists-group-title">{{ t('key.favoritePlaylistsTitle') }}</div>
           <div 
             v-for="playlist in favoritePlaylists.slice(0, 5)" 
             :key="`fav-${playlist.id}`"
@@ -49,7 +49,7 @@
       <div class="user-info" @click="handleUserClick" :class="{ 'is-login': isLoggedIn }">
         <img :src="userAvatar" alt="用户头像" class="user-avatar" />
         <div class="user-details">
-          <span class="username">{{ username || t('sidebar.notLoggedIn') }}</span>
+          <span class="username">{{ username || t('key.notLoggedIn') }}</span>
         </div>
       </div>
     </div>
@@ -83,16 +83,16 @@ const myPlaylists = ref([])
 const favoritePlaylists = ref([])
 
 const navItems = computed(() => [
-  { key: 'home', label: t('sidebar.home'), icon: 'M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z' },
-  { key: 'search', label: t('sidebar.search'), icon: 'M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z' },
-  { key: 'ranking', label: t('sidebar.ranking'), icon: 'M16 6l2.29 2.29-4.88 4.88-4-4L2 16.59 3.41 18l6-6 4 4 6.3-6.29L22 12V6z' },
-  { key: 'latest', label: t('sidebar.latest'), icon: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z' },
-  { key: 'library', label: t('sidebar.library'), icon: 'M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z' },
-  { key: 'favorites', label: t('sidebar.favorites'), icon: 'M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z' },
-  { key: 'favorite-playlists', label: t('sidebar.favoritePlaylists'), icon: 'M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z M17 12h-2v2h-2v-2h-2v-2h2V8h2v2h2v2z' },
-  { key: 'recent', label: t('sidebar.recentPlay'), icon: 'M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z' },
-  { key: 'playlists', label: t('sidebar.myPlaylists'), icon: 'M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8 12.5v-9l6 4.5-6 4.5z' },
-  { key: 'settings', label: t('sidebar.settings'), icon: 'M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z' }
+  { key: 'home', label: t('key.home'), icon: 'M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z' },
+  { key: 'search', label: t('key.search'), icon: 'M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z' },
+  { key: 'ranking', label: t('key.ranking'), icon: 'M16 6l2.29 2.29-4.88 4.88-4-4L2 16.59 3.41 18l6-6 4 4 6.3-6.29L22 12V6z' },
+  { key: 'latest', label: t('key.latest'), icon: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z' },
+  { key: 'library', label: t('key.library'), icon: 'M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z' },
+  { key: 'favorites', label: t('key.favorites'), icon: 'M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z' },
+  { key: 'favorite-playlists', label: t('key.favoritePlaylists'), icon: 'M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z M17 12h-2v2h-2v-2h-2v-2h2V8h2v2h2v2z' },
+  { key: 'recent', label: t('key.recentPlay'), icon: 'M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z' },
+  { key: 'playlists', label: t('key.myPlaylists'), icon: 'M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8 12.5v-9l6 4.5-6 4.5z' },
+  { key: 'settings', label: t('key.settings'), icon: 'M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z' }
 ])
 
 const userAvatar = computed(() => {
